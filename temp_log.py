1147b6a7fdfa36d8741e3ebaf4f2ec1374b02993
Taylor <taylorgswayze@gmail.com>
2018-10-20 01:27:10 -0400

    test

--- temp_log.py
+++ temp_log.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import dht11
 import time
 import datetime
@@ -14,14 +15,16 @@ instance = dht11.DHT11(pin=4)
 
 while True:
     result = instance.read()
-    timestamp = datetime.date.time.now()
+    timestamp = datetime.datetime.now()
+    tempf = (result.temperature * 9/5)+32
     if result.is_valid():
         print("Last valid input: " + str(datetime.datetime.now()))
-        print("Temperature: %d C" % result.temperature)
+        print("Temperature: %d f" % ((result.temperature * 9/5)+32))
         print("Humidity: %d %%" % result.humidity)
-    with open('temp_log.csv', mode='w') as temp_log:
-        temp_writer = csv.writer('temp_log', delimiter=',', quotechar='"')
-        temp_writer.writerow([timestamp, result])
-
-    time.sleep(1)
+   	with open('temp_log1.csv', mode='w') as temp_log:
+        	temp_writer = csv.writer(temp_log, delimiter=',', quotechar='"')
+        	for row in temp_log:
+			timestamp.append(row[0])
+			tempf.append(row[1])
+    time.sleep(5)
 
