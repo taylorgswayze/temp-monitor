168a5bf8fb5a65520267ecd850e6c878ab4c9dd7
Taylor <taylorgswayze@gmail.com>
2018-10-20 02:17:44 -0400

    final working version

--- temp_log.py
+++ temp_log.py
@@ -16,13 +16,13 @@ instance = dht11.DHT11(pin=4)
 while True:
     result = instance.read()
     timestamp = datetime.datetime.now()
-    tempf = (result.temperature * 9/5)+32
-    with open('temp_log.csv', mode='w+') as temp_log:
+    tempf = str((result.temperature * 9/5)+32)
+    with open('temp_log.csv', mode='a') as temp_log:
         temp_writer = csv.writer(temp_log, delimiter=',', quotechar='"')
-    if result.is_valid():
-        print("Last valid input: " + str(datetime.datetime.now()))
-        print("Temperature: %d f" % ((result.temperature * 9/5)+32))
-        print("Humidity: %d %%" % result.humidity)
-        temp_writer.writerow([timestamp, tempf])
-    time.sleep(5)
+    	if result.is_valid():
+        	print("Last valid input: " + str(datetime.datetime.now()))
+        	print("Temperature: %d f" % ((result.temperature * 9/5)+32))
+        	print("Humidity: %d %%" % result.humidity)
+        	temp_writer.writerow([timestamp, tempf + 'f'])
+    time.sleep(60)
 
