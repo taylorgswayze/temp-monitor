9f424d85535623343175f6afd3e509a9d9ed5335
Taylor Swayze <taylorgswayze@gmail.com>
2018-10-20 01:34:57 -0400

    Update temp_log.py

--- temp_log.py
+++ temp_log.py
@@ -21,10 +21,12 @@ while True:
         print("Last valid input: " + str(datetime.datetime.now()))
         print("Temperature: %d f" % ((result.temperature * 9/5)+32))
         print("Humidity: %d %%" % result.humidity)
-   	with open('temp_log1.csv', mode='w') as temp_log:
-        	temp_writer = csv.writer(temp_log, delimiter=',', quotechar='"')
-        	for row in temp_log:
-			timestamp.append(row[0])
-			tempf.append(row[1])
-    time.sleep(5)
 
+while True:
+    with open('temp_log1.csv', mode='w') as temp_log:
+        temp_writer = csv.writer(temp_log, delimiter=',', quotechar='"')
+        for row in temp_log:
+        timestamp.append(row[0])
+        tempf.append(row[1])
+    
+    time.sleep(5)
